a1f9809b3218d85f974a6a4742964cab26433c76
Natig Agharzayev <7p7ya11@code.edu.az>
2022-10-27 23:21:10 +0400

    modified main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ red.addEventListener('mouseover', () => {
     red.style.backgroundColor = 'pink';
     red.style.border = '2px blue solid';
 });
+
+red.addEventListener('mouseout', () => {
+    red.style.backgroundColor = 'yellow';
+    red.style.border = '2px purple solid';
+});
